164c5958bb647c3f3a5271b74029243ab6841b0f
Jaime Sánchez <jsancgui@myuax.com>
2024-03-27 00:44:11 +0100

    Creando la clase usuario

---
+++ app/src/main/java/es/sanchez/jaime/statusme/Usuario.java
@@ -0,0 +1,9 @@
+package es.sanchez.jaime.statusme;
+
+public class Usuario {
+    private int id;
+    private String name;
+    private String lastnme; // Make a Split for separate the name to the lastname
+    private String mail; // Make a control for caracters
+    private String password; // Make a system that controls the caracters
+}
