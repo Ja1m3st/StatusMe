18af816280900363059569a4714b2ac99f6f7e33
Jaime Sánchez <jsancgui@myuax.com>
2024-03-26 02:37:42 +0100

    casi termiando , terminado el modo oscuro

--- app/src/main/java/es/sanchez/jaime/statusme/Main_Login.java
+++ app/src/main/java/es/sanchez/jaime/statusme/Main_Login.java
@@ -1,12 +1,16 @@
 package es.sanchez.jaime.statusme;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.CompoundButton;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class Main_Login extends AppCompatActivity {
     Button myButton;
@@ -17,6 +21,20 @@ public class Main_Login extends AppCompatActivity {
         setContentView(R.layout.activity_main_login);
 
         myButton = findViewById(R.id.Login);
+        SwitchMaterial switchDarkMode = findViewById(R.id.DarkMode);
+
+        switchDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Si el interruptor está activado, cambia al modo oscuro
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    // Si el interruptor está desactivado, cambia al modo claro (predeterminado)
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
     }
 
     public void onClick(View view) {
