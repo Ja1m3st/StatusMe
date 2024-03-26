5830a364af67d58c58bc43e077014b7d5d0c3de4
Jaime Sánchez <jsancgui@myuax.com>
2024-03-26 01:23:35 +0100

    casi termiando

--- app/src/main/java/es/sanchez/jaime/statusme/Main_Login.java
+++ app/src/main/java/es/sanchez/jaime/statusme/Main_Login.java
@@ -3,12 +3,26 @@ package es.sanchez.jaime.statusme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
 public class Main_Login extends AppCompatActivity {
+    Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_login);
+
+        myButton = findViewById(R.id.Login);
+    }
+
+    public void onClick(View view) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.button_scale);
+        view.startAnimation(animation);
+
+        // Aquí puedes agregar cualquier otra acción que desees realizar cuando se presione el botón.
     }
 }
