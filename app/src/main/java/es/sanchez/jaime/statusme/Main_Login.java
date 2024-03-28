8855bdee5cbfa4b4269bcdcde73182f8d524e49b
Jaime Sánchez <jsancgui@myuax.com>
2024-03-28 19:57:05 +0100

    Iniciar Sesion con Google Hecho

--- app/src/main/java/es/sanchez/jaime/statusme/Main_Login.java
+++ app/src/main/java/es/sanchez/jaime/statusme/Main_Login.java
@@ -6,7 +6,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -19,13 +18,11 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.switchmaterial.SwitchMaterial;
@@ -91,6 +88,7 @@ public class Main_Login extends AppCompatActivity implements View.OnClickListene
                 login();
             }
         });
+
     }
 
     // Método para iniciar sesión con Auth
