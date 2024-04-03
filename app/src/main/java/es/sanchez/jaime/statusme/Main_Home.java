62ec5186837d3f2fe58248c2624ec21e1a6818bd
Jaime Sánchez <jsancgui@myuax.com>
2024-04-03 20:11:22 +0200

    Topnav y bottom nav en proceso

--- app/src/main/java/es/sanchez/jaime/statusme/Main_Home.java
+++ app/src/main/java/es/sanchez/jaime/statusme/Main_Home.java
@@ -1,8 +1,12 @@
 package es.sanchez.jaime.statusme;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Main_Home extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class Main_Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home);
+
+        // Obtener la referencia del TextView
+        TextView fechaTextView = findViewById(R.id.fechaTextView);
+
+        // Obtener la fecha actual
+        Date fechaActual = new Date();
+
+        // Formatear la fecha en el formato deseado
+        SimpleDateFormat formatoFecha = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        String fechaFormateada = formatoFecha.format(fechaActual);
+
+        // Establecer la fecha formateada en el TextView
+        fechaTextView.setText(fechaFormateada);
     }
 }
