a84178605846e2bdbba0ca2f7cc832718958721c
Jaime Sánchez <jsancgui@myuax.com>
2024-04-06 18:57:42 +0200

    bottom nav + colores pret

--- app/src/main/java/es/sanchez/jaime/statusme/Main_Usuario.java
+++ app/src/main/java/es/sanchez/jaime/statusme/Main_Usuario.java
@@ -1,14 +1,14 @@
 package es.sanchez.jaime.statusme;
 
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class Main_Usuario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_usuario);
     }
 }
