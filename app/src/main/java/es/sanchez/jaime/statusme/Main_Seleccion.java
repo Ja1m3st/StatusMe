5af69fc6761dd1bd5206fce70d584bb64adce054
Jaime Sánchez <jsancgui@myuax.com>
2024-04-22 20:35:11 +0200

    botones ouestos

--- app/src/main/java/es/sanchez/jaime/statusme/Main_Seleccion.java
+++ app/src/main/java/es/sanchez/jaime/statusme/Main_Seleccion.java
@@ -3,27 +3,55 @@ package es.sanchez.jaime.statusme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CheckBox;
 
 import java.util.ArrayList;
 
-public class Main_Seleccion extends AppCompatActivity {
+public class Main_Seleccion extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_seleccion);
 
-        CheckBox feliz = findViewById(R.id.feliz);
-        String selecionado= "";
-        String bien = "Bien";
-        String mal = "Mal";
+        CheckBox checkBoxFeliz = findViewById(R.id.feliz);
+        CheckBox checkBoxMedio = findViewById(R.id.medio);
+        CheckBox checkBoxMal = findViewById(R.id.mal);
 
-        if (feliz.isChecked()) {
-            selecionado = "Bien";
-        }
+        checkBoxFeliz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBoxFeliz.isChecked()) {
+                    checkBoxMedio.setChecked(false);
+                    checkBoxMal.setChecked(false);
+                }
+            }
+        });
+
+        checkBoxMedio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBoxMedio.isChecked()) {
+                    checkBoxFeliz.setChecked(false);
+                    checkBoxMal.setChecked(false);
+                }
+            }
+        });
+
+        checkBoxMal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBoxMal.isChecked()) {
+                    checkBoxFeliz.setChecked(false);
+                    checkBoxMedio.setChecked(false);
+                }
+            }
+        });
     }
 
+    @Override
+    public void onClick(View v) {
 
-
+    }
 }
