01d260691916764d2c1b361ed0d345d84b998047
Jaime Sánchez <jsancgui@myuax.com>
2024-05-17 18:12:38 +0200

    calendario

--- app/src/main/java/es/sanchez/jaime/statusme/Main_Calendario.java
+++ app/src/main/java/es/sanchez/jaime/statusme/Main_Calendario.java
@@ -1,10 +1,16 @@
 package es.sanchez.jaime.statusme;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class Main_Calendario extends AppCompatActivity {
 
@@ -12,6 +18,24 @@ public class Main_Calendario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_calendario);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        // Obtener la fecha actual
+        Calendar calendar = Calendar.getInstance();
+        long currentTime = calendar.getTimeInMillis();
+
+        // Establecer la fecha actual en el CalendarView
+        calendarView.setDate(currentTime, false, true);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // month is 0-indexed, so add 1 to it if you need 1-indexed month
+                String date = dayOfMonth + "/" + (month + 1) + "/" + year;
+                Toast.makeText(Main_Calendario.this, "Selected date: " + date, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void onClick(View view) {
